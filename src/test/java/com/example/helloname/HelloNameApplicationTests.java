ef3cd402715a6b0448f264f836613f0240ebdb67
mikhi-mh <hmikhilesh@gmail.com>
2023-06-08 18:58:40 +0530

    Initial Commit

---
+++ src/test/java/com/example/helloname/HelloNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloNameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
